5b04aba68816b929b19660a9d2360d5985e35657
= <=>
2022-08-29 22:48:16 +0300

    Initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,20 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+const Expenses = ({ items }) => {
+  return (
+    <Card className="expenses">
+      {items.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        ></ExpenseItem>
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
