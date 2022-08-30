1266cd8b6bea1585d868e768fd114b4e491091da
Kaan Ünlü <nihatkaanunlu@gmail.com>
2022-08-30 21:03:29 +0300

    Remove button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,14 @@
-import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ title, amount, date }) => {
-  const [titleState, setTitleState] = useState(title);
-
-  const clickHandler = () => {
-    setTitleState("Updated!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{titleState}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
-        <button type="button" onClick={clickHandler}>
-          Change Title
-        </button>
       </div>
     </Card>
   );
