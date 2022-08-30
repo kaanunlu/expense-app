eafd04341563f59ab516f3e28b9b33f12a8bc586
Kaan Ünlü <nihatkaanunlu@gmail.com>
2022-08-30 22:40:00 +0300

    Add filter structure, code cleanup

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,9 +6,28 @@ import { useState } from "react";
 
 const Expenses = ({ items }) => {
   const [filteredyear, setFilteredYear] = useState("2020");
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredyear;
+  });
+
+  let expenseContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((item) => (
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      ></ExpenseItem>
+    ));
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -16,14 +35,7 @@ const Expenses = ({ items }) => {
           selected={filteredyear}
           onChangeFilter={filterChangeHandler}
         />
-        {items.map((item) => (
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          ></ExpenseItem>
-        ))}
+        {expenseContent}
       </Card>
     </div>
   );
